dde2e448a39d003a32c5e502249a6cb7d88d2729
chpassos <crlshnrqpassos@gmail.com>
2024-12-16 10:20:13 -0300

    Camera rotates with mouse

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,12 +1,22 @@
 extends CharacterBody3D
 
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Captures mouse movement
 var _mouse_input: bool = false # is the mouse moving?
 var _rotation_input: float
 var _tilt_input: float 
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+var _mouse_rotation: Vector3
+
+
+@export var TILT_LOWER_LIMIT := deg_to_rad(-90.0)
+@export var TILT_UPPER_LIMIT := deg_to_rad(90.0)
+@export var CAMERA_CONTROLLER: Node3D
+
+
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED # Hide mouse cursor
@@ -18,14 +28,30 @@ func _unhandled_input(event): # Built-in function. Fires evtime mouse moves
 	if _mouse_input == true:
 		_rotation_input = -event.relative.x
 		_tilt_input = -event.relative.y
-	
-	print(Vector2(_rotation_input, _tilt_input))
-	
 
+
+
+func _update_camera(delta):
+	
+	# Rotate camera using euler rotation
+	_mouse_rotation.x += _tilt_input * delta
+	_mouse_rotation.x = clamp(_mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
+	_mouse_rotation.y += _rotation_input * delta
+	
+	CAMERA_CONTROLLER.transform.basis = Basis.from_euler(_mouse_rotation)
+	CAMERA_CONTROLLER.rotation.z = 0.0
+	
+	_rotation_input = 0.0
+	_tilt_input = 0.0
 
 
 
 func _physics_process(delta):
+	_update_camera(delta)
+	
+	
+	
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
